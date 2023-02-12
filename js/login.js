889abc28f373210abb0b805351729b92fe63e37e
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-02-12 10:46:27 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // get the email address inside the email field
+    const emailField = document.getElementById('user-mail');
+    const email = emailField.value;
+    // console.log(email);
+    const passField = document.getElementById('user-pass');
+    const password = passField.value;
+    // console.log(email);
+    // console.log(password);
+
+    if (email === 'amarbap@gmail.com' && password === '12345') {
+        console.log('valid!');
+    }
+    else {
+        console.log('invalid!');
+    }
+})
